1fed12d7c3b9061e1415af7c3d706b5d49aef331
DoVT-coder <pha8654@gmail.com>
2021-06-23 23:24:29 +0700

    update search controller.php

--- sever/controller.php
+++ sever/controller.php
@@ -11,6 +11,26 @@ if ($action == "deleteShare"){
   $delete_editshId = $_GET["shId"];
   deleteShare($delete_editshId);
 }
+else if($action == "searchTable1"){
+    $keyword = $_GET["keyword"];
+    searchTable1($keyword);
+  }
+  else if($action == "searchTable2"){
+    $keyword = $_GET["keyword"];
+    searchTable2($keyword);
+  }
+  else if($action == "cSearchTable1"){
+    $category = $_GET["category"];
+    $time = $_GET["time"];
+    $planguage = $_GET["planguage"];
+    cSearchTable1($category,$time,$planguage);
+  }
+  else if($action == "cSearchTable2"){
+    $category = $_GET["category"];
+    $time = $_GET["time"];
+    $planguage = $_GET["planguage"];
+    cSearchTable2($category,$time,$planguage);
+  }
 
 function deleteShare($delete_editshId)
 {
@@ -36,5 +56,135 @@ function deleteShare($delete_editshId)
         echo "Error deleting record:". $conn->error;
     }
     $conn->close();
+}
+
+//search
+function searchTable1($keyword){
+    //Get data from database
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "subshare";
+    //Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn -> set_charset("utf8");
+
+    //Check connection
+    if($conn ->connect_error){
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = "SELECT share.pName as pName, customer.nName as nName, share.pLanguage as pLanguage, share.tShare as tShare 
+    FROM share, customer WHERE pName LIKE '%$keyword%' LIMIT 100;";
+    // echo $sql;
+    // die();
+    
+    $result = $conn->query($sql);
+    // var_dump ($result);
+    // die();
+    if ($result->num_rows > 0) {
+        // Convert $result to json format
+        $data = $result->fetch_all(MYSQLI_ASSOC);
+        // var_dump($data);
+        // die();
+        echo json_encode($data);
+      } else {
+        echo "{result: \"No result found\"}";
+      }
+    $conn->close();
+  }
+  
+function searchTable2($keyword){
+    //Get data from database
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "subshare";
+    //Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn -> set_charset("utf8");
+
+    //Check connection
+    if($conn ->connect_error){
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = "SELECT request.pName as pName, customer.nName as nName, request.pLanguage as pLanguage, request.tRequest as tRequest, request.price as price FROM request, customer WHERE pName LIKE '%$keyword%' LIMIT 100;";
+    // echo $sql;
+    // die();
+    
+    $result = $conn->query($sql);
+    // var_dump ($result);
+    // die();
+    if ($result->num_rows > 0) {
+        // Convert $result to json format
+        $data = $result->fetch_all(MYSQLI_ASSOC);
+        // var_dump($data);
+        // die();
+        echo json_encode($data);
+      } else {
+        echo "{result: \"No result found\"}";
+      }
+    $conn->close();
+  }
+  
+function cSearchTable1($category,$time,$planguage){
+  //Get data from database
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "subshare";
+  //Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  $conn -> set_charset("utf8");
+
+  //Check connection
+  if($conn ->connect_error){
+      die("Connection failed: " . $conn->connect_error);
+  }
+  $sql = "SELECT share.pName as pName, customer.nName as nName, share.pLanguage as pLanguage, share.tShare as tShare 
+  FROM share, customer WHERE share.category = '$category' AND YEAR(share.tShare) = $time AND share.pLanguage = '$planguage';";
+  $result = $conn->query($sql);
+  // var_dump ($result);
+  // die();
+  if ($result->num_rows > 0) {
+      // Convert $result to json format
+      $data = $result->fetch_all(MYSQLI_ASSOC);
+      // var_dump($data);
+      // die();
+      echo json_encode($data);
+    } else {
+      echo "{result: \"No result found\"}";
+    }
+  $conn->close();
+}
+
+function cSearchTable2($category,$time,$planguage){
+  //Get data from database
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "subshare";
+  //Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  $conn -> set_charset("utf8");
+
+  //Check connection
+  if($conn ->connect_error){
+      die("Connection failed: " . $conn->connect_error);
+  }
+  $sql = "SELECT request.pName as pName, customer.nName as nName, request.pLanguage as pLanguage, request.tRequest as tRequest, request.price as price FROM request, customer 
+  WHERE request.category = '$category' AND YEAR(request.tRequest) = $time AND request.pLanguage = '$planguage';";
+  $result = $conn->query($sql);
+  // var_dump ($result);
+  // die();
+  if ($result->num_rows > 0) {
+      // Convert $result to json format
+      $data = $result->fetch_all(MYSQLI_ASSOC);
+      // var_dump($data);
+      // die();
+      echo json_encode($data);
+    } else {
+      echo "{result: \"No result found\"}";
+    }
+  $conn->close();
 }
 ?>
