38331f26e76c38fe7023b575853b1cd9d6872c90
Tuong Vy <58203954+KhanhTuong1211@users.noreply.github.com>
2021-06-23 15:50:59 +0700

    add

---
+++ sever/controler.php
@@ -0,0 +1,54 @@
+<?php
+
+class actionjs
+{
+    public $sheId;
+    public $pName;
+    public $cuId;
+    public $pLanguage;
+    public $category;
+    public $imagesLink;
+    public $videoLink;
+    public $price;
+    public $info;
+    public $tRequest;
+    public $private;
+}
+
+//link test ?action=deleteShare&shId=4
+
+$action = $_GET["action"];
+// echo($action);
+// die();
+
+if ($action == "deleteShare"){
+  $actionjsshId = $_GET["shId"];
+  deleteShare($actionjsshId);
+}
+
+function deleteShare($actionjsshId)
+{
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "subshare";
+    
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn -> set_charset("utf8");
+    
+    if($conn ->connect_error){
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = "DELETE FROM share WHERE shId = $actionjsshId";
+    // echo $sql;
+    // die();
+        
+    if ($conn->query($sql) === TRUE) {
+        echo "Record deleted successfully";
+    } 
+    else {
+        echo "Error deleting record:". $conn->error;
+    }
+    $conn->close();
+}
+?>
