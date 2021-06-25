a85e0cc21af166017d2a54f5db1f1f0dd2b5fe9b
DoVT-coder <19521365@gm.uit.edu.vn>
2021-06-25 14:39:13 +0700

    update profile

--- client/js/home.js
+++ client/js/home.js
@@ -1,3 +1,4 @@
+//FIXME:Chau
 function table1() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
@@ -5,16 +6,16 @@ function table1() {
             var searchResult = JSON.parse(this.responseText);
             document.getElementById("table_1").innerHTML = "";
             for (iResult in searchResult) {
-                var text = "<tr><td id='getLink' class='getLink' scope='row' onclick='getLinkShare()'>" + searchResults[iResult].pName+ "</td><td id='getLinkProfile' class='getLink' onclick='getLinkProfile()'>" + searchResults[iResult].nName + "</td><td>" + searchResults[iResult].tShare + "</td><td>" + searchResults[iResult].pLanguage+ "</td></tr>";
+                var text = "<tr><td id='getLink' class='getLink' scope='row' onclick='getLinkShare()'>" + searchResults[iResult].pName + "</td><td id='getLinkProfile' class='getLink' onclick='getLinkProfile()'>" + searchResults[iResult].nName + "</td><td>" + searchResults[iResult].tShare + "</td><td>" + searchResults[iResult].pLanguage + "</td></tr>";
                 document.getElementById("table_1").innerHTML += text;
             }
         }
     };
-        
-    xhttp.open("GET", "http://localhost/webSubShare/server/controller.php?action=loadHomeCategory",true);
+
+    xhttp.open("GET", "http://localhost/webSubShare/server/controller.php?action=loadHomeCategory", true);
     xhttp.send();
 }
-
+//FIXME:Chau
 function table2() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
@@ -22,16 +23,16 @@ function table2() {
             var searchResult = JSON.parse(this.responseText);
             document.getElementById("table_2").innerHTML = "";
             for (iResult in searchResult) {
-                var text = "<tr><td id='getLinkRequest' class='getLink' scope='row' onclick='getLinkRequest()'>" + searchResults[iResult].pName+ "</td><td id='getLinkProfile' class='getLink' onclick='getLinkProfile()'>" + searchResults[iResult].nName + "</td><td>" + searchResults[iResult].tRequest + "</td><td>" + searchResults[iResult].pLanguage+ "</td><td>" + searchResults[iResult].price+ "</td></tr>";
+                var text = "<tr><td id='getLinkRequest' class='getLink' scope='row' onclick='getLinkRequest()'>" + searchResults[iResult].pName + "</td><td id='getLinkProfile' class='getLink' onclick='getLinkProfile()'>" + searchResults[iResult].nName + "</td><td>" + searchResults[iResult].tRequest + "</td><td>" + searchResults[iResult].pLanguage + "</td><td>" + searchResults[iResult].price + "</td></tr>";
                 document.getElementById("table_2").innerHTML += text;
             }
         }
     };
-        
-    xhttp.open("GET", "http://localhost/webSubShare/server/controller.php?action=loadHomeCategory",true);
+
+    xhttp.open("GET", "http://localhost/webSubShare/server/controller.php?action=loadHomeCategory", true);
     xhttp.send();
 }
-
+//FIXME:Chau
 function table3() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
@@ -39,17 +40,17 @@ function table3() {
             var searchResult = JSON.parse(this.responseText);
             document.getElementById("table_3").innerHTML = "";
             for (iResult in searchResult) {
-                var text = "<tr><td id='getLinkProfile' class='getLink' scope='row' onclick='getLinkProfile()'>" + searchResults[iResult].nName + "</td><td>" + searchResults[iResult].rate+ "</td></tr>";
+                var text = "<tr><td id='getLinkProfile' class='getLink' scope='row' onclick='getLinkProfile()'>" + searchResults[iResult].nName + "</td><td>" + searchResults[iResult].rate + "</td></tr>";
                 document.getElementById("table_3").innerHTML += text;
             }
         }
     };
-        
-    xhttp.open("GET", "http://localhost/webSubShare/server/controller.php?action=loadHomeCategory",true);
+
+    xhttp.open("GET", "http://localhost/webSubShare/server/controller.php?action=loadHomeCategory", true);
     xhttp.send();
 
 }
-
+//FIXME:Chau
 function table4() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
@@ -57,16 +58,16 @@ function table4() {
             var searchResult = JSON.parse(this.responseText);
             document.getElementById("table_4").innerHTML = "";
             for (iResult in searchResult) {
-                var text = "<tr><td id='getLinkRequest' class='getLink' scope='row' onclick='getLinkRequest()'>" + searchResults[iResult].pName+ "</td><td id='getLinkProfile' class='getLink' onclick='getLinkProfile()'>" + searchResults[iResult].nName + "</td><td>" + searchResults[iResult].rate+ "</td></tr>";
+                var text = "<tr><td id='getLinkRequest' class='getLink' scope='row' onclick='getLinkRequest()'>" + searchResults[iResult].pName + "</td><td id='getLinkProfile' class='getLink' onclick='getLinkProfile()'>" + searchResults[iResult].nName + "</td><td>" + searchResults[iResult].rate + "</td></tr>";
                 document.getElementById("table_4").innerHTML += text;
             }
         }
     };
-        
-    xhttp.open("GET", "http://localhost/webSubShare/server/controller.php?action=loadHomeCategory",true);
+
+    xhttp.open("GET", "http://localhost/webSubShare/server/controller.php?action=loadHomeCategory", true);
     xhttp.send();
 }
-
+//FIXME:chau
 function table5() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
@@ -74,13 +75,13 @@ function table5() {
             var searchResult = JSON.parse(this.responseText);
             document.getElementById("table_5").innerHTML = "";
             for (iResult in searchResult) {
-                var text = "<tr><td id='getLinkProfile' class='getLink' onclick='getLinkProfile()'>" + searchResults[iResult].rate+ "</td></tr>";
+                var text = "<tr><td id='getLinkProfile' class='getLink' onclick='getLinkProfile()'>" + searchResults[iResult].rate + "</td></tr>";
                 document.getElementById("table_5").innerHTML += text;
             }
         }
     };
-        
-    xhttp.open("GET", "http://localhost/webSubShare/server/controller.php?action=loadHomeCategory",true);
+
+    xhttp.open("GET", "http://localhost/webSubShare/server/controller.php?action=loadHomeCategory", true);
     xhttp.send();
 }
 onload = function home() {
--- client/js/search.js
+++ client/js/search.js
@@ -3,90 +3,87 @@ function table1() {
     var keyword = document.getElementById("txtKeyword").value;
     // Make Ajax request
     var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function(){
-      if(this.readyState == 4 && this.status == 200){
-        var searchResults = JSON.parse(this.responseText);
-        document.getElementById("table_1").innerHTML = "";
-        for (var iResult in searchResults)
-        {
-          var text = "<tr><td id='getLink' class='getLink' scope='row' onclick='getLinkShare()'>" + searchResults[iResult].pName+ "</td><td id='getLinkProfile' class='getLink' onclick='getLinkProfile()'>" + searchResults[iResult].nName + "</td><td>" + searchResults[iResult].tShare + "</td><td>" + searchResults[iResult].pLanguage+ "</td></tr>";
-          document.getElementById("table_1").innerHTML += text;
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var searchResults = JSON.parse(this.responseText);
+            document.getElementById("table_1").innerHTML = "";
+            for (var iResult in searchResults) {
+                var text = "<tr><td id='getLink' class='getLink' scope='row' onclick='getLinkShare()'>" + searchResults[iResult].pName + "</td><td id='getLinkProfile' class='getLink' onclick='getLinkProfile()'>" + searchResults[iResult].nName + "</td><td>" + searchResults[iResult].tShare + "</td><td>" + searchResults[iResult].pLanguage + "</td></tr>";
+                document.getElementById("table_1").innerHTML += text;
+            }
         }
-      }
     };
     xhttp.open("GET", "http://localhost/SubShare/server/controler.php?action=searchTable1&keyword=" + keyword, true);
     xhttp.send();
-  }
-  
-  function table2() {
+}
+
+function table2() {
     // Send Request to sever
     var keyword = document.getElementById("txtKeyword").value;
     // Make Ajax request
     var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function(){
-      if(this.readyState == 4 && this.status == 200){
-        var searchResults = JSON.parse(this.responseText);
-        document.getElementById("table_2").innerHTML = "";
-        for (var iResult in searchResults)
-        {
-          var text = "<tr><td id='getLink' class='getLink' scope='row' onclick='getLinkShare()'>" + searchResults[iResult].pName+ "</td><td id='getLinkProfile' class='getLink' onclick='getLinkProfile()'>" + searchResults[iResult].nName + "</td><td>" + searchResults[iResult].tRequest + "</td><td>" + searchResults[iResult].pLanguage+"</td><td>" + searchResults[iResult].price+ "</td></tr>";
-          document.getElementById("table_2").innerHTML += text;
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var searchResults = JSON.parse(this.responseText);
+            document.getElementById("table_2").innerHTML = "";
+            for (var iResult in searchResults) {
+                var text = "<tr><td id='getLink' class='getLink' scope='row' onclick='getLinkShare()'>" + searchResults[iResult].pName + "</td><td id='getLinkProfile' class='getLink' onclick='getLinkProfile()'>" + searchResults[iResult].nName + "</td><td>" + searchResults[iResult].tRequest + "</td><td>" + searchResults[iResult].pLanguage + "</td><td>" + searchResults[iResult].price + "</td></tr>";
+                document.getElementById("table_2").innerHTML += text;
+            }
         }
-      }
     };
-  xhttp.open("GET", "http://localhost/SubShare/server/controler.php?action=searchTable2&keyword=" + keyword, true);
-  xhttp.send();
-  }
-  function cTable1() {
+    xhttp.open("GET", "http://localhost/SubShare/server/controler.php?action=searchTable2&keyword=" + keyword, true);
+    xhttp.send();
+}
+
+function cTable1() {
     var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function(){
-      if(this.readyState == 4 && this.status == 200){
-        var searchResults = JSON.parse(this.responseText);
-        document.getElementById("table_1").innerHTML = "";
-        for (var iResult in searchResults)
-        {
-          var text = "<tr><td id='getLink' class='getLink' scope='row' onclick='getLinkShare()'>" + searchResults[iResult].pName+ "</td><td id='getLinkProfile' class='getLink' onclick='getLinkProfile()'>" + searchResults[iResult].nName + "</td><td>" + searchResults[iResult].tShare + "</td><td>" + searchResults[iResult].pLanguage+ "</td></tr>";
-          document.getElementById("table_1").innerHTML += text;
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var searchResults = JSON.parse(this.responseText);
+            document.getElementById("table_1").innerHTML = "";
+            for (var iResult in searchResults) {
+                var text = "<tr><td id='getLink' class='getLink' scope='row' onclick='getLinkShare()'>" + searchResults[iResult].pName + "</td><td id='getLinkProfile' class='getLink' onclick='getLinkProfile()'>" + searchResults[iResult].nName + "</td><td>" + searchResults[iResult].tShare + "</td><td>" + searchResults[iResult].pLanguage + "</td></tr>";
+                document.getElementById("table_1").innerHTML += text;
+            }
         }
-      }
     };
     var category = document.getElementById("category").value;
     var time = document.getElementById("time").value;
     var planguage = document.getElementById("planguage").value;
-  
-    xhttp.open("GET", "http://localhost/SubShare/server/controler.php?action=cSearchTable1&category="+category+"&time="+time+"&planguage="+planguage);
+
+    xhttp.open("GET", "http://localhost/SubShare/server/controler.php?action=cSearchTable1&category=" + category + "&time=" + time + "&planguage=" + planguage);
     xhttp.send();
-  }
-  
-  function cTable2(str) {
+}
+
+function cTable2(str) {
     var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function(){
-      if(this.readyState == 4 && this.status == 200){
-        var searchResults = JSON.parse(this.responseText);
-        document.getElementById("table_2").innerHTML = "";
-        for (var iResult in searchResults)
-        {
-          var text = "<tr><td id='getLink' class='getLink' scope='row' onclick='getLinkShare()'>" + searchResults[iResult].pName+ "</td><td id='getLinkProfile' class='getLink' onclick='getLinkProfile()'>" + searchResults[iResult].nName + "</td><td>" + searchResults[iResult].tRequest + "</td><td>" + searchResults[iResult].pLanguage+"</td><td>" + searchResults[iResult].price+ "</td></tr>";
-          document.getElementById("table_2").innerHTML += text;
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var searchResults = JSON.parse(this.responseText);
+            document.getElementById("table_2").innerHTML = "";
+            for (var iResult in searchResults) {
+                var text = "<tr><td id='getLink' class='getLink' scope='row' onclick='getLinkShare()'>" + searchResults[iResult].pName + "</td><td id='getLinkProfile' class='getLink' onclick='getLinkProfile()'>" + searchResults[iResult].nName + "</td><td>" + searchResults[iResult].tRequest + "</td><td>" + searchResults[iResult].pLanguage + "</td><td>" + searchResults[iResult].price + "</td></tr>";
+                document.getElementById("table_2").innerHTML += text;
+            }
         }
-      }
     };
     var category = document.getElementById("category").value;
     var time = document.getElementById("time").value;
     var planguage = document.getElementById("planguage").value;
-  
-    xhttp.open("GET", "http://localhost/SubShare/server/controler.php?action=cSearchTable2&category="+category+"&time="+time+"&planguage="+planguage);
+
+    xhttp.open("GET", "http://localhost/SubShare/server/controler.php?action=cSearchTable2&category=" + category + "&time=" + time + "&planguage=" + planguage);
     xhttp.send();
-  }
-  function search() {
+}
+
+function search() {
     //Send Request to sever
     table1();
     table2();
-  }
-  
-  function cSearch() {
+}
+
+function cSearch() {
     //Send Request to sever
     cTable1();
     cTable2();
-  }
-  
+}
