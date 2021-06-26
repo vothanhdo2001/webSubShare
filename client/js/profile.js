0e949c2d797ddce62c2338a9260d5e3dfb3cba61
DoVT-coder <19521365@gm.uit.edu.vn>
2021-06-26 21:48:28 +0700

    update action.js

--- client/js/profile.js
+++ client/js/profile.js
@@ -6,7 +6,7 @@ function table1() {
             document.getElementById("table_1").innerHTML = "";
             for (var iResult in searchResults) {
                 document.cookie = searchResults[iResult].shId;
-                var text = "<tr><td class='text-center d-none d-xs-none'>" + searchResults[iResult].shId + "</td><td id='getLink' class='getLink' scope='row' onclick='getLinkShare()'>" + searchResults[iResult].pName + "</td><td>" + searchResults[iResult].tShare + "</td><td>" + searchResults[iResult].pLanguage + "<td><button onclick='deleteShare(" + searchResults[iResult].shId + ")' class='btn btn-danger'><a class='m-0 text-center text-white'>Xoá</a></button></td><td><button onclick='editShare(" + searchResults[iResult].shId + ")' class='btn btn-success'><a class='m-0 text-center text-white' href='../narbar/editShare.html'>Cập nhật</a></button></td></tr>";
+                var text = "<tr><td class='text-center d-none d-xs-none'>" + searchResults[iResult].shId + "</td><td id='getLink' class='getLink' scope='row' onclick='getLinkShare()'>" + searchResults[iResult].pName + "</td><td>" + searchResults[iResult].tShare + "</td><td>" + searchResults[iResult].pLanguage + "<td><button onclick='deleteShare(" + searchResults[iResult].shId + ")' class='btn btn-danger'><a class='m-0 text-center text-white'>Xoá</a></button></td><td><button onclick='getCookie(" + searchResults[iResult].shId + ")' class='btn btn-success'><a class='m-0 text-center text-white' href='../narbar/editShare.html'>Cập nhật</a></button></td></tr>";
                 document.getElementById("table_1").innerHTML += text;
             }
         }
@@ -23,7 +23,7 @@ function table2() {
             var searchResults = JSON.parse(this.responseText);
             document.getElementById("table_2").innerHTML = "";
             for (var iResult in searchResults) {
-                var text = "<tr><td class='text-center d-none d-xs-none'>" + searchResults[iResult].reId + "<td id='getLink' class='getLink' scope='row' onclick='getLinkShare()'>" + searchResults[iResult].pName + "</td><td>" + searchResults[iResult].tRequest + "</td><td>" + searchResults[iResult].pLanguage + "<td><button onclick='deleteRequest(" + searchResults[iResult].reId + ")' class='btn btn-danger'><a class='m-0 text-center text-white'>Xoá</button></td><td><button onclick='editRequest(" + searchResults[iResult].reId + ")' class='btn btn-success'><a class='m-0 text-center text-white' href='../narbar/editRequest.html'>Cập nhật</a></button></td></tr>";
+                var text = "<tr><td class='text-center d-none d-xs-none'>" + searchResults[iResult].reId + "<td id='getLink' class='getLink' scope='row' onclick='getLinkShare()'>" + searchResults[iResult].pName + "</td><td>" + searchResults[iResult].tRequest + "</td><td>" + searchResults[iResult].pLanguage + "<td><button onclick='deleteRequest(" + searchResults[iResult].reId + ")' class='btn btn-danger'><a class='m-0 text-center text-white'>Xoá</button></td><td><button onclick='getCookie(" + searchResults[iResult].reId + ")' class='btn btn-success'><a class='m-0 text-center text-white' href='../narbar/editRequest.html'>Cập nhật</a></button></td></tr>";
                 document.getElementById("table_2").innerHTML += text;
             }
         }
