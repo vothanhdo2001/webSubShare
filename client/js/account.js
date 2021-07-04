71747606005810b0fef47e725812275a52111c30
Tuong Vy <58203954+KhanhTuong1211@users.noreply.github.com>
2021-07-04 17:15:17 +0700

    a little

--- client/js/account.js
+++ client/js/account.js
@@ -6,7 +6,43 @@ function setCookie(cname, cvalue, exdays) {
 }
 
 function register() {
-    //TODO: viếT theo phương thức post
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+
+        var searchResults = JSON.parse(this.responseText);
+        var iResult = searchResults[0].cuId;
+        if (iResult <= 0) {
+            var text = '<div class="card bg-danger border-1 h-100 "> <div class="card-body p-0 p-lg-0 pt-3 pt-lg-3 "> <p><b>Tài khoản đã tồn tại !!!</b></p> </div> </div>';
+            document.getElementById("notification").innerHTML += text;
+        }
+        else{
+            var text = '<div class="card bg-success border-1 h-100 "> <div class="card-body p-0 p-lg-0 pt-3 pt-lg-3 "> <p><b>Đăng ký thành công</b></p> </div> </div>';
+            document.getElementById("notification").innerHTML += text;
+            setTimeout(function() { window.location = "/webSubShare/client/account/login.html";}, 1000);
+        }
+    }
+    }
+    var nName = document.getElementById("nName").value;
+    var sex = document.getElementById("sex").value;
+    var mail = document.getElementById("mail").value;
+    var pass = document.getElementById("pass").value;
+    var introduce = document.getElementById("introduce").value;
+    var cpass = document.getElementById("cpass").value;
+    
+    if(pass == cpass)
+    {
+        var url = "/webSubShare/server/controller.php?action=register&nName=" + nName + "&sex=" + sex + "&mail=" + mail + "&pass=" + pass + "&introduce=" + introduce;
+        xhttp.open("GET", url, true);
+        
+        xhttp.send();
+    }
+    else {
+        alert("Đăng ký thất bại");
+        // var text = '<div class="card bg-danger border-1 h-100 "> <div class="card-body p-0 p-lg-0 pt-3 pt-lg-3 "> <p><b>Thông tin đăng nhập không chính xác !!!</b></p> </div> </div>';
+        // document.getElementById("notification").innerHTML += text;
+    }
 
 }
 
